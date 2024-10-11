28372db0509bf4ee6ef9c641e02489572600462c
Leandro Damasio <ldamasio@gmail.com>
2024-10-11 16:48:18 -0300

    feat: Add name field

--- axie-front/app/busca-singular/page.tsx
+++ axie-front/app/busca-singular/page.tsx
@@ -18,6 +18,8 @@ type AxieSchemaType = z.infer<typeof formSchema>;
 
 export default function BuscaSingular() {
   const [searchResult, setSearchResult] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   const {
     register,
@@ -25,13 +27,31 @@ export default function BuscaSingular() {
     formState: { errors }
   } = useForm<AxieSchemaType>({ resolver: zodResolver(formSchema) });
 
+  const onSubmit: SubmitHandler<AxieSchemaType> = async (data) => {
+    setLoading(true);
+    setError(null); // Reset error state
 
-  const onSubmit: SubmitHandler<AxieSchemaType> = (data) => {
-    console.log(data);
+    try {
+      const response = await fetch('http://localhost:8000/axie-name/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ axieId: data.axieId.toString() }), // Convert axieId to string
+      });
 
-    setSearchResult(`Resultado da busca: ${data.axieId}`);
+      if (!response.ok) {
+        throw new Error('Erro ao buscar informações do Axie.');
+      }
+
+      const result = await response.json();
+      setSearchResult(`Nome do Axie: ${result.axie_name}`); // Access axie_name from response
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
   }
-
 
   return (
     <>
@@ -43,9 +63,7 @@ export default function BuscaSingular() {
             Busca singular de Axie
           </h2>
 
-
           <form onSubmit={handleSubmit(onSubmit)} className="form">
-
             <Input
               className="input"
               placeholder="axieId"
@@ -56,16 +74,22 @@ export default function BuscaSingular() {
               <Button
                 type="submit"
                 className="transition-transform transform active:scale-95"
-              >Buscar</Button>
+                disabled={loading} // Disable button while loading
+              >
+                {loading ? 'Buscando...' : 'Buscar'}
+              </Button>
               {errors.axieId && (
                 <span className="text-red-500 mt-2">{errors.axieId.message}</span>
               )}
             </div>
           </form>
 
-
           <div className="space-y-8 w-full max-w-md mx-auto">
-            {searchResult ? searchResult : 'Resultado da busca: '}
+            {error ? (
+              <span className="text-red-500">{error}</span>
+            ) : (
+              searchResult || 'Resultado da busca: '
+            )}
           </div>
 
         </div>
@@ -73,5 +97,3 @@ export default function BuscaSingular() {
     </>
   );
 }
-
-
